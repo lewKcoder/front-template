0b9aa7e69b41e6bec6903491f3091e36378019e6
lewKcoder <7kamimura74@gmail.com>
2023-08-17 00:32:41 +0900

    Fixed inputs

--- src/layouts/inputs/inputs.tsx
+++ src/layouts/inputs/inputs.tsx
@@ -1,12 +1,19 @@
 import { FunctionComponent } from 'react';
 import { inputNames } from './data';
-import styles from './styles.module.scss';
 import { ElementLayout } from '@/layouts/elements-layout';
+import styles from './styles.module.scss';
 
 export const Inputs: FunctionComponent = () => {
   return (
     <ElementLayout>
-      <div className={styles.container}></div>
+      <div className={styles.container}>
+        <input type="text" className={styles.form_1} placeholder="Hello!!" />
+        <input type="text" className={styles.form_2} placeholder="Hello!!" />
+        <input type="text" className={styles.form_3} placeholder="Hello!!" />
+        <input type="text" className={styles.form_4} placeholder="Hello!!" />
+        <input type="text" className={styles.form_5} placeholder="Hello!!" />
+        <input type="text" className={styles.form_6} placeholder="Hello!!" />
+      </div>
     </ElementLayout>
   );
 };
