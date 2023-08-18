93568f8a8e2308a0f2df2fbb1c4fbeeb73fde436
lewKcoder <7kamimura74@gmail.com>
2023-08-19 01:13:50 +0900

    Added new pattern

--- src/layouts/inputs-checkbox/inputs-checkbox.tsx
+++ src/layouts/inputs-checkbox/inputs-checkbox.tsx
@@ -144,6 +144,100 @@ export const InputsCheckbox: FunctionComponent = () => {
             ></polyline>
           </svg>
         </div>
+        <div className={styles.checkbox_15}>
+          <label className={styles.label}>
+            <input type="checkbox" className={styles.checkbox} />
+            <span className={styles.svg_content}>
+              <svg
+                aria-hidden="true"
+                className={styles.icon_checkbox}
+                width="28px"
+                height="28px"
+                viewBox="0 0 28 28"
+                version="1"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M4 14l8 7L24 7"></path>
+              </svg>
+            </span>
+            <p className={styles.checkbox_textwrapper}>Checkbox</p>
+          </label>
+        </div>
+        <div className={styles.checkbox_16}>
+          <input type="checkbox" className={styles.checkbox} id="Checkbox16" />
+          <label className={styles.label} htmlFor="Checkbox16"></label>
+        </div>
+        <div className={styles.checkbox_17}>
+          <label className={styles.label}>
+            <input type="checkbox" className={styles.checkbox} />
+            <span className={styles.span}></span>
+          </label>
+        </div>
+        <div className={styles.checkbox_18}>
+          <label className={styles.label}>
+            <input type="checkbox" className={styles.checkbox} />
+          </label>
+        </div>
+        <div className={styles.checkbox_19}>
+          <label className={styles.label}>
+            <input type="checkbox" className={styles.checkbox} />
+            <div>
+              <svg viewBox="0 0 44 44">
+                <path
+                  d="M14,24 L21,31 L39.7428882,11.5937758 C35.2809627,6.53125861 30.0333333,4 24,4 C12.95,4 4,12.95 4,24 C4,35.05 12.95,44 24,44 C35.05,44 44,35.05 44,24 C44,19.3 42.5809627,15.1645919 39.7428882,11.5937758"
+                  transform="translate(-2.000000, -2.000000)"
+                ></path>
+              </svg>
+            </div>
+          </label>
+        </div>
+        <div className={styles.checkbox_20}>
+          <input type="checkbox" id="Checkbox20" className={styles.checkbox} />
+          <label className={styles.label} htmlFor="Checkbox20">
+            <span>
+              <svg width="12px" height="10px" viewBox="0 0 12 10">
+                <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
+              </svg>
+            </span>
+            <span>Checkbox</span>
+          </label>
+        </div>
+        <div className={styles.checkbox_21}>
+          <input type="checkbox" id="Checkbox21" className={styles.checkbox} />
+          <label htmlFor="Checkbox21" className={styles.label}>
+            <svg width="18px" height="18px" viewBox="0 0 18 18">
+              <path d="M1,9 L1,3.5 C1,2 2,1 3.5,1 L14.5,1 C16,1 17,2 17,3.5 L17,14.5 C17,16 16,17 14.5,17 L3.5,17 C2,17 1,16 1,14.5 L1,9 Z"></path>
+              <polyline points="1 9 7 14 15 4"></polyline>
+            </svg>
+          </label>
+        </div>
+        <div className={styles.checkbox_22}>
+          <input type="checkbox" id="Checkbox22" className={styles.checkbox} />
+          <label htmlFor="Checkbox22" className={styles.label}>
+            Check this
+          </label>
+        </div>
+        <div className={styles.checkbox_23}>
+          <label className={styles.label}>
+            <input type="checkbox" className={styles.checkbox} />
+          </label>
+        </div>
+        <div className={styles.checkbox_24}>
+          <div className={styles.block}>
+            <input type="checkbox" id="Checkbox24" className={styles.checkbox} />
+            <label htmlFor="Checkbox24" className={styles.label}></label>
+          </div>
+        </div>
+        <div className={styles.checkbox_25}>
+          <input type="checkbox" id="Checkbox25" className={styles.checkbox} />
+          <label htmlFor="Checkbox25" className={styles.toggle}>
+            <span>
+              <svg width="10px" height="10px" viewBox="0 0 10 10">
+                <path d="M5,1 L5,1 C2.790861,1 1,2.790861 1,5 L1,5 C1,7.209139 2.790861,9 5,9 L5,9 C7.209139,9 9,7.209139 9,5 L9,5 C9,2.790861 7.209139,1 5,1 L5,9 L5,1 Z"></path>
+              </svg>
+            </span>
+          </label>
+        </div>
       </div>
     </ElementLayout>
   );
