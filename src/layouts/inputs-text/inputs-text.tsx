b4e9fbae68c44512443773f3ef4d27be9d762b15
lewKcoder <7kamimura74@gmail.com>
2023-08-18 12:28:18 +0900

    Added new pattern

--- src/layouts/inputs-text/inputs-text.tsx
+++ src/layouts/inputs-text/inputs-text.tsx
@@ -7,12 +7,49 @@ export const InputsText: FunctionComponent = () => {
   return (
     <ElementLayout>
       <div className={styles.container}>
-        <input type="text" className={styles.form_1} placeholder="Hello!!" />
-        <input type="text" className={styles.form_2} placeholder="Hello!!" />
-        <input type="text" className={styles.form_3} placeholder="Hello!!" />
-        <input type="text" className={styles.form_4} placeholder="Hello!!" />
-        <input type="text" className={styles.form_5} placeholder="Hello!!" />
-        <input type="text" className={styles.form_6} placeholder="Hello!!" />
+        <input type="text" className={styles.form_1} placeholder="form_1" />
+        <input type="text" className={styles.form_2} placeholder="form_2" />
+        <input type="text" className={styles.form_3} placeholder="form_3" />
+        <input type="text" className={styles.form_4} placeholder="form_4" />
+        <input type="text" className={styles.form_5} placeholder="form_5" />
+        <input type="text" className={styles.form_6} placeholder="form_6" />
+        <input type="text" className={styles.form_7} placeholder="form_7" />
+        <input type="text" className={styles.form_8} placeholder="form_8" />
+        <input type="text" className={styles.form_9} placeholder="form_9" />
+        <input type="text" className={styles.form_10} placeholder="form_10" />
+        <input type="text" className={styles.form_11} placeholder="form_11" />
+        <input type="text" className={styles.form_12} placeholder="form_12" />
+        <input type="text" className={styles.form_13} placeholder="form_13" />
+        <div className={styles.form_14}>
+          <input type="text" id="Form14" placeholder="form_14" className={styles.input} />
+          <label htmlFor="Form14" className={styles.label}>
+            form_14
+          </label>
+        </div>
+        <div className={styles.form_15}>
+          <input type="text" id="Form15" placeholder="form_15" className={styles.input} />
+          <label htmlFor="Form15" className={styles.label}>
+            form_15
+          </label>
+        </div>
+        <div className={styles.form_16}>
+          <input type="text" placeholder="form_16" className={styles.input} />
+          <i className={styles.icon} aria-hidden="true"></i>
+        </div>
+        <div className={styles.form_17}>
+          <i className={styles.icon} aria-hidden="true"></i>
+          <input type="text" placeholder="form_17" className={styles.input} />
+        </div>
+        <div className={styles.form_18}>
+          <input type="text" id="Form18" className={styles.input} />
+          <label htmlFor="Form18" className={styles.label}>
+            form_18
+          </label>
+        </div>
+        <div className={styles.form_19}>
+          <input type="text" placeholder="form_19" className={styles.input} />
+          <span className={styles.underline} />
+        </div>
       </div>
     </ElementLayout>
   );
