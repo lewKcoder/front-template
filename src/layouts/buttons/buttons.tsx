785e590113dc014e4b525f17c1c1a2bfc8256737
lewKcoder <7kamimura74@gmail.com>
2023-08-18 18:51:23 +0900

    Fixed buttons

--- src/layouts/buttons/buttons.tsx
+++ src/layouts/buttons/buttons.tsx
@@ -1,5 +1,4 @@
 import { FunctionComponent } from 'react';
-import { buttonNames, withSpanButtonNames } from './data';
 import styles from './styles.module.scss';
 import { ElementLayout } from '@/layouts/elements-layout';
 
@@ -7,24 +6,83 @@ export const Buttons: FunctionComponent = () => {
   return (
     <ElementLayout>
       <div className={styles.container}>
-        {buttonNames.map((name) => (
-          <button key={name} className={styles[`button_${name}`]}>
-            button_{name}
-          </button>
-        ))}
-
-        {withSpanButtonNames.map((name) => (
-          <button key={name} className={styles[`button_${name}`]}>
-            <span>button_{name}</span>
-          </button>
-        ))}
-
-        <button className={styles.button_icon_animation}>
-          <i>icon</i>button_icon_animation
+        <button className={styles.button_1}>Button 1</button>
+        <button className={styles.button_2}>Button 2</button>
+        <button className={styles.button_3}>Button 3</button>
+        <button className={styles.button_4}>Button 4</button>
+        <button className={styles.button_5}>Button 5</button>
+        <button className={styles.button_6}>Button 6</button>
+        <button className={styles.button_7}>Button 7</button>
+        <button className={styles.button_8}>Button 8</button>
+        <button className={styles.button_9}>Button 9</button>
+        <button className={styles.button_10}>Button 10</button>
+        <button className={styles.button_11}>Button 11</button>
+        <button className={styles.button_12}>Button 12</button>
+        <button className={styles.button_13}>Button 13</button>
+        <button className={styles.button_14}>Button 14</button>
+        <button className={styles.button_15}>Button 15</button>
+        <button className={styles.button_16}>Button 16</button>
+        <button className={styles.button_17}>Button 17</button>
+        <button className={styles.button_18}>Button 18</button>
+        <button className={styles.button_19}>Button 19</button>
+        <button className={styles.button_20}>Button 20</button>
+        <button className={styles.button_21}>Button 21</button>
+        <button className={styles.button_22}>Button 22</button>
+        <button className={styles.button_23}>Button 23</button>
+        <button className={styles.button_24}>Button 24</button>
+        <button className={styles.button_25}>
+          <span className={styles.text}>Button 25</span>
         </button>
-        <button className={styles.button_text_change}>
-          <span className={styles.original}>button_text_change</span>
-          <span className={styles.hover}>Click me!</span>
+        <button className={styles.button_26}>Button 26</button>
+        <button className={styles.button_27}>Button 27</button>
+        <button className={styles.button_28}>Button 28</button>
+        <button className={styles.button_29}>Button 29</button>
+        <button className={styles.button_30}>Button 30</button>
+        <button className={styles.button_31}>Button 31</button>
+        <button className={styles.button_32}>Button 32</button>
+        <button className={styles.button_33}>
+          <span className={styles.text}>Button 33</span>
+          <span>Click me</span>
+        </button>
+        <button className={styles.button_34}>Button 34</button>
+        <button className={styles.button_35}>
+          <span className={styles.text}>Button 35</span>
+        </button>
+        <button className={styles.button_36}>Button 36</button>
+        <button className={styles.button_37}>Button 37</button>
+        <button className={styles.button_38}>Button 38</button>
+        <button className={styles.button_39}>Button 39</button>
+        <button className={styles.button_40}>
+          <span className={styles.text}>Button 40</span>
+        </button>
+        <button className={styles.button_41}>
+          <span className={styles.text}>Button 41</span>
+        </button>
+        <button className={styles.button_42}>Button 42</button>
+        <button className={styles.button_43}>Button 43</button>
+        <button className={styles.button_44}>Button 44</button>
+        <button className={styles.button_45}>
+          <span className={styles.shadow}></span>
+          <span className={styles.edge}></span>
+          <span className={`${styles.front} ${styles.text}`}>Button 45</span>
+        </button>
+        <button className={styles.button_46}>Button 46</button>
+        <button className={styles.button_47}>Button 47</button>
+        <button className={styles.button_48}>Button 48</button>
+        <button className={styles.button_49}>Button 49</button>
+        <button className={styles.button_50}>Button 50</button>
+        <button className={styles.button_51}>Button 51</button>
+        <button className={styles.button_52}>Button 52</button>
+        <button className={styles.button_53}>Button 53</button>
+        <button className={styles.button_54}>Button 54</button>
+        <button className={styles.button_55}>
+          <span className={styles.text}>Button 55</span>
+        </button>
+        <button className={styles.button_56}>Button 56</button>
+        <button className={styles.button_57}>Button 57</button>
+        <button className={styles.button_58}>Button 58</button>
+        <button className={styles.button_59}>
+          <span className={styles.text}>Button 59</span>
         </button>
       </div>
     </ElementLayout>
