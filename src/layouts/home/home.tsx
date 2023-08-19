b8846216879c3fe981519ba67d8312ad14bf45de
lewKcoder <7kamimura74@gmail.com>
2023-08-19 16:18:04 +0900

    Replace home page

---
+++ src/layouts/home/home.tsx
@@ -0,0 +1,35 @@
+import { FunctionComponent } from 'react';
+import { menuItems } from '@/const/urls';
+import { Inter } from 'next/font/google';
+import styles from './styles.module.scss';
+import Link from 'next/link';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const Home: FunctionComponent = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.description}>
+        <p>各種要素のスタイリングやReact, Nextjsをハンズオンで確認するためのプロジェクト</p>
+        <div>
+          <a href="https://twitter.com/lew_yumei" target="_blank" rel="noopener noreferrer">
+            By @lew_yumei
+          </a>
+        </div>
+      </div>
+
+      <div className={styles.center}>
+        <div className={styles.grid}>
+          {menuItems.map(({ href, label, description }) => (
+            <Link key={label} href={href} className={styles.card}>
+              <h2 className={inter.className}>
+                {label} <span>-&gt;</span>
+              </h2>
+              <p className={inter.className}>{description}</p>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,5 @@
 import Head from 'next/head';
-import { Inter } from 'next/font/google';
-import styles from '@/styles/Home.module.scss';
-import Link from 'next/link';
-import { menuItems } from '@/const/urls';
-
-const inter = Inter({ subsets: ['latin'] });
+import { Home as HomeLayout } from '@/layouts/home';
 
 export default function Home() {
   return (
@@ -13,29 +8,7 @@ export default function Home() {
         <title>Front template</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>各種要素のスタイリングやReact, Nextjsをハンズオンで確認するためのプロジェクト</p>
-          <div>
-            <a href="https://twitter.com/lew_yumei" target="_blank" rel="noopener noreferrer">
-              By @lew_yumei
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <div className={styles.grid}>
-            {menuItems.map(({ href, label, description }) => (
-              <Link key={label} href={href} className={styles.card}>
-                <h2 className={inter.className}>
-                  {label} <span>-&gt;</span>
-                </h2>
-                <p className={inter.className}>{description}</p>
-              </Link>
-            ))}
-          </div>
-        </div>
-      </main>
+      <HomeLayout />
     </>
   );
 }
